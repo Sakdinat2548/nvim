f3a0e199c4a95439598089ba5c126167a28a9295
Sakdinat2548 <sakdinat.kuy@gmail.com>
2025-05-16 05:15:42 +0700

    hellowww

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "bashls", "lua_ls", "rust_analyzer", "vimls", "ts_ls" },
+            }
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.diagnostic.config({ virtual_text = true,})
+            vim.lsp.enable('lua_ls')
+            vim.lsp.enable('ts_ls')
+            vim.lsp.enable('vimls')
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        end
+    }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "dockerfile", "php", "python" },
+            ensure_installed = { "bash", "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "dockerfile", "php", "python" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },  
